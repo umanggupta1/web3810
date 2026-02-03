26a770de598eb4f8050d65ed5e159395748eb444
umanggupta1 <umanggupta@mail.weber.edu>
2026-02-03 14:14:20 -0700

    Change response text

--- ch5/sample-app/app.js
+++ ch5/sample-app/app.js
@@ -2,6 +2,6 @@ app.get('/', (req, res) => {
 if (lookupFeatureToggle(req, "HOME_PAGE_FLAVOR") === "v2") {
 res.send(newFancyHomepage());
 } else {
-res.send('Hello, World!');
+res.send('Fundamentals of DevOps!');
 }
 });
