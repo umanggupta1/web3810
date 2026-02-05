5c9d57d5e6cc2a5a0594e33b969ba54af4e20090
umanggupta1 <umanggupta@mail.weber.edu>
2026-02-05 13:35:21 -0700

    Update response text in test

--- ch5/sample-app/app.test.js
+++ ch5/sample-app/app.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
-const app = require('./app');                             
+const app = require('./app');
 
-describe('Test the app', () => {                          
+describe('Test the app', () => {
 
-  test('Get / should return Hello, World!', async () => { 
-    const response = await request(app).get('/');         
-    expect(response.statusCode).toBe(200);                
-    expect(response.text).toBe('Hello, World!');          
+  test('Get / should return Hello, World!', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('Fundamentals of DevOps!');
   });
 
   test('Get /name/Bob should return Hello, Bob!', async () => {
