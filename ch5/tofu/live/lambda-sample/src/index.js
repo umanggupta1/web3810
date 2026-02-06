2556f92d71c3123ef03952df258231b69b52ff91
umanggupta1 <umanggupta@mail.weber.edu>
2026-02-06 13:47:23 -0700

    Fix: Update Lambda handler for Function URL compatibility

--- ch5/tofu/live/lambda-sample/src/index.js
+++ ch5/tofu/live/lambda-sample/src/index.js
@@ -1,3 +1,9 @@
-exports.handler = (event, context, callback) => {
-  callback(null, {statusCode: 200, body: "Hello, World!"});
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/plain'
+    },
+    body: "Hello, World!"
+  };
 };
