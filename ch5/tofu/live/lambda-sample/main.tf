a1df7ea2135a4ae6184de15f272415d66406a39b
umanggupta1 <umanggupta@mail.weber.edu>
2026-02-06 13:39:15 -0700

    Fix: Remove quotes from var.name in Lambda module

--- ch5/tofu/live/lambda-sample/main.tf
+++ ch5/tofu/live/lambda-sample/main.tf
@@ -2,11 +2,16 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "name" {
+  description = "Name of the Lambda function"
+  type        = string
+}
+
 module "function" {
   source  = "brikis98/devops/book//modules/lambda"
   version = "1.0.0"
 
-  name = "var.name"         
+  name = var.name
 
   src_dir = "${path.module}/src" 
   runtime = "nodejs20.x"         
@@ -22,3 +27,7 @@ module "function" {
   create_url = true
 }
 
+output "function_url" {
+  description = "URL of the Lambda function"
+  value       = module.function.function_url
+}
