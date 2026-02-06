ae840335d3acb4c2f7c38634268643501079b697
umanggupta1 <umanggupta@mail.weber.edu>
2026-02-06 14:23:35 -0700

    final test for screenshot

--- ch5/tofu/live/ci-cd-permissions/main.tf
+++ ch5/tofu/live/ci-cd-permissions/main.tf
@@ -5,26 +5,25 @@ provider "aws" {
 module "oidc_provider" {
   source = "../../modules/github-aws-oidc"
 
-  provider_url = "https://token.actions.githubusercontent.com" 
+  provider_url = "https://token.actions.githubusercontent.com"
 
 }
 
 module "iam_roles" {
   source = "../../modules/gh-actions-iam-roles"
 
-  name              = "lambda-sample"                           
-  oidc_provider_arn = module.oidc_provider.oidc_provider_arn    
+  name              = "lambda-sample"
+  oidc_provider_arn = module.oidc_provider.oidc_provider_arn
 
-  enable_iam_role_for_testing = true                            
+  enable_iam_role_for_testing = true
 
   # TODO: fill in your own repo name here!
-  github_repo      = "CodySquadroni/devops-book" 
-  lambda_base_name = "lambda-sample"                            
-
-  enable_iam_role_for_plan  = true                                
-  enable_iam_role_for_apply = true                                
+  github_repo               = "umanggupta1/web3810"
+  lambda_base_name          = "lambda-sample"
+  enable_iam_role_for_plan  = true
+  enable_iam_role_for_apply = true
 
   # TODO: fill in your own bucket and table name here!
-  tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
-  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
+  tofu_state_bucket         = "fundamentals-of-devops-tofu-state"
+  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state"
 }
